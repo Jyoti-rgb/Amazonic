a048ecca8d3ece25dcb30e7bd4fbaacbb50e7001
Jyoti-rgb <jyotimahor6@gmail.com>
2024-02-20 11:18:47 +0530

    Start Backend Server

---
+++ backend/data.js
@@ -0,0 +1,124 @@
+// import Img1 from "./assets/images/img_1.jpg";
+// import Img2 from "./assets/images/img_2.jpg";
+// import Img3 from "./assets/images/img_3.jpg";
+// import Img4 from "./assets/images/img_4.jpg";
+// import Img5 from "./assets/images/img_5.jpg";
+// import Img6 from "./assets/images/img_6.jpg";
+// import Img7 from "./assets/images/img_7.jpg";
+// import Img8 from "./assets/images/img_4.jpg";
+
+const data = {
+  products: [
+    {
+      _id: "1",
+      name: "Women Floral Print Anarkali Kurta with Pant and Dupatta",
+      category: "Weston dress",
+      image: "../frontend/src/assets/images/img_1.jpg",
+      price: 120,
+      countInStock: 10,
+      brand: "Allen Solly",
+      rating: 4.5,
+      numReview: 13,
+      description: "high quality product",
+    },
+    {
+      _id: "2",
+      name: "AASK Kurta for Women Dress",
+      category: "Weston dress",
+      // image: Img2,
+      image: "../frontend/src/assets/images/img_2.jpg",
+
+      price: 90,
+      countInStock: 10,
+      brand: "Allen Solly",
+      rating: 5,
+      numReview: 20,
+      description: "high quality product",
+    },
+    {
+      _id: "3",
+      name: " AAYU FIT & Flare Georgette Calf Length Dress for Women",
+      category: "Weston dress",
+      // image: Img3,
+      image: "../frontend/src/assets/images/img_3.jpg",
+
+      price: 180,
+      countInStock: 50,
+      brand: "Allen Solly",
+      rating: 3,
+      numReview: 10,
+      description: "high quality product",
+    },
+    {
+      _id: "4",
+      name: "Sheetal Associates Women Casual Regular Sleeves Crepe Solid Fit and Flare Dress",
+      category: "Weston dress",
+      // image: Img4,
+      image: "../frontend/src/assets/images/img_1.jpg",
+
+      price: 120,
+      countInStock: 15,
+      brand: "Allen Solly",
+      rating: 4.5,
+      numReview: 50,
+      description: "high quality product",
+    },
+    {
+      _id: "5",
+      name: "QUEEN ELLIE Women's A-Line Midi Dress",
+      category: "Weston dress",
+      // image: Img5,
+      image: "../frontend/src/assets/images/img_5.jpg",
+
+      price: 100,
+      countInStock: 0,
+      brand: "Allen Solly",
+      rating: 4.5,
+      numReview: 34,
+      description: "high quality product",
+    },
+    {
+      _id: "6",
+      name: "Western Dresses for Women",
+      category: "Weston dress",
+      // image: Img6,
+      image: "../frontend/src/assets/images/img_6.jpg",
+
+      price: 70,
+      countInStock: 25,
+      brand: "Allen Solly",
+      rating: 4.5,
+      numReview: 15,
+      description: "high quality product",
+    },
+    {
+      _id: "7",
+      name: "QUEEN ELLIE Women's A-Line Midi Dress",
+      category: "Weston dress",
+      // image: Img7,
+      image: "../frontend/src/assets/images/img_7.jpg",
+
+      price: 200,
+      countInStock: 4,
+      brand: "QUEEN",
+      rating: 4.5,
+      numReview: 7,
+      description: "high quality product",
+    },
+    {
+      _id: "8",
+      name: "AASK Kurta for Women Dress",
+      category: "Weston dress",
+      // image: Img8,
+      image: "../frontend/src/assets/images/img_8.jpg",
+
+      price: 230,
+      countInStock: 9,
+      brand: "Allen Solly",
+      rating: 4.5,
+      numReview: 8,
+      description: "high quality product",
+    },
+  ],
+};
+export default data;
---
+++ backend/server.js
@@ -0,0 +1,17 @@
+import express from "express";
+import data from "./data.js";
+// const express = require("express");
+// const data = require("./data");
+const app = express();
+
+app.get("/data", (req, res) => {
+  res.send(data.products);
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello web Server");
+});
+
+app.listen("5000", (req, res) => {
+  console.log("Server have started on 5000 port");
+});
